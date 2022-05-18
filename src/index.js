0faec753e9e415699b58442becc5ee7774e1d20a
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-05-18 12:32:14 -0500

    Retos parte 1 listos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider} from 'react-intl';
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+	<IntlProvider locale="en">
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
